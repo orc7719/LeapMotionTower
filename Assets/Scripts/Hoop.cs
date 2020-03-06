5b76bf5d3ff165fef78f338b8bc129f7b4f71dc9
Cory McAdam <orc7719@gmail.com>
2020-03-06 13:57:50 +0000

    HOOP

---
+++ Assets/Scripts/Hoop.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hoop : MonoBehaviour
+{
+    public int hoopSize = 0;
+
+    public bool eject = false;
+
+    public void DropHoop()
+    {
+        if(eject)
+        {
+            eject = false;
+            GetComponent<Rigidbody>().AddForce(Vector3.up * 10);
+        }
+    }
+}
---
+++ Assets/Scripts/PoleController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoleController : MonoBehaviour
+{
+    public List<Hoop> hoops = new List<Hoop>();
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.transform.root.tag == "Hoop")
+        {
+            Hoop hoop = other.transform.root.GetComponent<Hoop>();
+
+            if (CheckHoop(hoop))
+                hoop.eject = true;
+
+
+            if (!hoops.Contains(hoop))
+                hoops.Add(hoop);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.transform.root.tag == "Hoop")
+        {
+            Hoop hoop = other.transform.root.GetComponent<Hoop>();
+            if (hoops.Contains(hoop))
+                hoops.Remove(hoop);
+        }
+    }
+
+    bool CheckHoop (Hoop hoopToCheck)
+    {
+        if (hoops.Count > 0)
+            if (hoops[hoops.Count].hoopSize < hoopToCheck.hoopSize)
+                return true;
+
+        return false;
+    }
+}
